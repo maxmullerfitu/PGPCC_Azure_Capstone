cb5779cd3262e00a78e0f4c30e670a0f18759c67
Ubuntu <azureuser@PythonDev.manyt2udkmvetejoww15lw5qgg.bx.internal.cloudapp.net>
2022-02-22 10:41:56 +0000

    Custom HTTP trigger

--- CustomHttpTrigger/__init__.py
+++ CustomHttpTrigger/__init__.py
@@ -4,8 +4,8 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
+    logging.info('PGPCC HTTP trigger function processed a request.')
+    message = "This function is a part of PGPCC Capstone project. Execution successfull."
     name = req.params.get('name')
     if not name:
         try:
@@ -16,9 +16,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. " + message)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             message + " Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
